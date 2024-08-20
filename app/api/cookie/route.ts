7810b774ec8f5e8bcd46444c5557444833cc334f
Janson Hartliep <jhartliep@wistia.com>
2024-08-20 08:56:50 -0500

    Simplest repro

---
+++ app/api/cookie/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+const COOKIE_NAME = '_api_route_cookie_';
+const VALUES = ['control', 'variantA', 'variantB'] as const;
+
+export const GET = async (): Promise<NextResponse> => {
+  const cookieStore = cookies();
+  console.log(cookieStore.getAll());
+  const setCookie = cookieStore.get(COOKIE_NAME);
+
+  if (setCookie) {
+    console.log(`Value found in cookie: ${setCookie.value}`);
+    return NextResponse.json({ setCookie });
+  }
+
+  console.log('Value not found in cookie.');
+  const randomValue = VALUES[(Math.floor(Math.random() * VALUES.length))]
+  console.log(`Selecting random value: ${randomValue}`);
+  cookies().set(COOKIE_NAME, randomValue, {
+    maxAge: 60 * 60 * 24 * 365, // 1 year
+  });
+  return NextResponse.json({ [COOKIE_NAME]: randomValue });
+};
+
+export const dynamic = 'force-dynamic';
